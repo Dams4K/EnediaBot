d89f60f9e983e6c1a5bca2249204c02045efa234
Dams4K <dams4k@users.noreply.github.com>
2023-04-27 19:36:06 +0200

    get_ceil_power2_size

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,9 +1,23 @@
 import io
+import math
 from discord import *
 from PIL import Image, ImageDraw, ImageFilter
 
 class WelcomeCog(Cog):
-    AVATAR_SIZE = 256
+    AVATAR_SIZE = 384
+
+    def get_ceil_power2_size(self, size):
+        """Returns the closer but higher power 2 number of the given size
+
+        Parameters
+        ----------
+            size: int
+
+        Returns
+        -------
+            int
+        """
+        return 2**math.ceil(math.log10(size)/math.log10(2))
 
     def __init__(self, bot):
         self.bot = bot
@@ -15,9 +29,11 @@ class WelcomeCog(Cog):
         template = Image.open("images/welcome_template.png")
 
         # Get member's avatar
-        member_avatar = member.display_avatar.with_size(WelcomeCog.AVATAR_SIZE)
+        member_avatar = member.display_avatar.with_size(self.get_ceil_power2_size(WelcomeCog.AVATAR_SIZE))
         avatar_bytes = await member_avatar.read()
         avatar_image = Image.open(io.BytesIO(avatar_bytes))
+        if avatar_image.size[0] != WelcomeCog.AVATAR_SIZE:
+            avatar_image = avatar_image.resize([WelcomeCog.AVATAR_SIZE]*2)
 
         avatar_mask = Image.new("L", avatar_image.size, 0)
         draw = ImageDraw.Draw(avatar_mask)
@@ -41,6 +57,12 @@ class WelcomeCog(Cog):
         if channel := await guild.fetch_channel(1101184040373076069):
                 await channel.send(file=await self.get_welcome_image(member))
         
-        
+
+    @Cog.listener() # TODO: REMOVE THIS
+    async def on_message(self, message):
+        if not message.author.bot:
+            await self.on_member_join(message.author)
+
+
 def setup(bot):
     bot.add_cog(WelcomeCog(bot))
