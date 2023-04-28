2747493c03217b385ec6891c846d8f8648219d8b
Dams4K <dams4k@users.noreply.github.com>
2023-04-28 19:23:06 +0200

    working welcome data (without commands)

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -6,108 +6,42 @@ from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from data_class import WelcomeConfig
 
 class WelcomeCog(Cog):
-    AVATAR_SIZE = 384
+    AVATAR_SIZE = 320
     FONT_SIZE = 86
 
     def __init__(self, bot):
         self.bot = bot
 
-    def get_ceil_power2_size(self, size):
-        """Returns the closer but higher power 2 number of the given size
-
-        Parameters
-        ----------
-            size: int
-
-        Returns
-        -------
-            int
-        """
-        return 2**math.ceil(math.log10(size)/math.log10(2))
-
-    def get_area(self, position: list, size: list):
-        return (position[0], position[1], position[0]+size[0], position[1]+size[1])
-
-    def get_text_dimensions(self, text_string, font):
-        # from https://stackoverflow.com/a/46220683/9263761
-        ascent, descent = font.getmetrics()
-
-        text_width = font.getmask(text_string).getbbox()[2]
-        text_height = font.getmask(text_string).getbbox()[3] + descent
-
-        return (text_width, text_height)
-
-    def fit_text_in(self, text, width, font) -> list:
-        result = []
-
-        remaining_text = ""
-        tries = 0
-
-        while not text == remaining_text == "":
-            dimension = self.get_text_dimensions(text, font)
-
-            if dimension[0] > width:
-                splitted_text = text.split()
-                
-                if len(splitted_text) == 1:
-                    remaining_text = text[-1] + remaining_text
-                    text = text[:-1]
-                else:
-                    text = " ".join(splitted_text[:-1])
-                    remaining_text = " " + splitted_text[-1] + remaining_text            
-            else:
-                result.append(text.lstrip())
-                text = remaining_text.lstrip()
-                remaining_text = ""
-
-        return result
-
-
-    async def get_welcome_image(self, member: Member):
-        template = Image.open("assets/images/welcome_template.png")
-
-        # Get member's avatar
-        member_avatar = member.display_avatar.with_size(self.get_ceil_power2_size(WelcomeCog.AVATAR_SIZE))
-        avatar_bytes = await member_avatar.read()
-        avatar_image = Image.open(io.BytesIO(avatar_bytes))
-        if avatar_image.size[0] != WelcomeCog.AVATAR_SIZE:
-            avatar_image = avatar_image.resize([WelcomeCog.AVATAR_SIZE]*2)
-
-        # Create avatar mask
-        avatar_mask = Image.new("L", avatar_image.size, 0)
-        draw = ImageDraw.Draw(avatar_mask)
-        draw.ellipse((0, 0, WelcomeCog.AVATAR_SIZE, WelcomeCog.AVATAR_SIZE), fill=255)
-        avatar_mask = avatar_mask.filter(ImageFilter.GaussianBlur(1))
-        
-        # Add avatar image
-        avatar_area = self.get_area([48]*2, [WelcomeCog.AVATAR_SIZE]*2)
-        template.paste(avatar_image, avatar_area, avatar_mask)
-
-        # Add text
-        font = ImageFont.truetype("assets/font/Roboto-Medium.ttf", WelcomeCog.FONT_SIZE)
-
-        text = f"{member} a rejoint le serveur"
-        # print(self.get_text_dimensions(text, font))
-        text = self.fit_text_in(text, template.size[0] - WelcomeCog.AVATAR_SIZE - 48 - 24, font)
-
-        template_draw = ImageDraw.Draw(template)
-        template_draw.multiline_text((avatar_area[2]+24, (avatar_area[1]+avatar_area[3])/2), "\n".join(text), font=font, fill=(255, 255, 255, 255), anchor="lm")
-
-        with io.BytesIO() as image_binary:
-            template.save(image_binary, "PNG")
-            image_binary.seek(0)
-            
-            file = File(image_binary, filename="welcome.png")
-            return file
-            
     @Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
         welcome_config = WelcomeConfig(guild)
 
         if channel := await welcome_config.fetch_channel():
-            await channel.send(f"Hey {member.mention}, Bienvenue sur **Enedia** !", file=await self.get_welcome_image(member))
+            with io.BytesIO() as image_binary:
+                img = await welcome_config.get_image(member)
+                img.save(image_binary, "PNG")
+                image_binary.seek(0)
+                
+                file = File(image_binary, filename="welcome.png")
+                await channel.send(welcome_config.get_message(member), file=file)
         
+    @Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        welcome_config = WelcomeConfig(message.guild)
+
+        with io.BytesIO() as image_binary:
+            img = await welcome_config.get_image(message.author)
+            img.save(image_binary, "PNG")
+            image_binary.seek(0)
+            
+            file = File(image_binary, filename="welcome.png")
+            await message.channel.send(file=file)
+
+
     welcome = SlashCommandGroup("welcome", default_member_permissions=Permissions(administrator=True), guild_only=True)
     w_set = welcome.create_subgroup("set")
 
@@ -119,7 +53,7 @@ class WelcomeCog(Cog):
     @w_set.command(name="message")
     @option("message", type=str, max_length=1024)
     async def set_message(self, ctx, message: str):
-        pass
+        ctx.welcome_config.set_channel(message)
 
 
 def setup(bot):
--- data_class/welcome_data.py
+++ data_class/welcome_data.py
@@ -1,12 +1,19 @@
 import discord
+import io
+import math
 from ddm import *
 from utils.references import References
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
 
 class WelcomeConfig(Saveable):
     def __init__(self, guild):
         self._guild = guild
 
         self.channel_id = self._guild.system_channel
+        self.message = "Hey {member.mention}, Bienvenue sur **{guild.name}** !"
+        
+        self.image = None
+        self.image_text = "{member} a rejoint le serveur"
 
         super().__init__(References.get_guild_folder(f"{self._guild.id}/welcome_config.json"))
     
@@ -31,3 +38,111 @@ class WelcomeConfig(Saveable):
             return await self._guild.fetch_channel(self.channel_id)
         except discord.NotFound:
             return self._guild.system_channel
+    
+    @Saveable.update()
+    def set_message(self, message: str):
+        self.message = message[:1024]
+    
+    def get_message(self, member: discord.Member) -> str:
+        return self.message.format(member=member, guild=self._guild)
+    
+    @Saveable.update()
+    def set_image_text(self, text: str) -> None:
+        self.image_text = text
+    
+    def get_image_text(self, member: discord.Member) -> str:
+        return self.image_text.format(member=member, guild=self._guild)
+
+    def get_ceil_power2_size(self, size):
+        """Returns the closer but higher power 2 number of the given size
+
+        Parameters
+        ----------
+            size: int
+
+        Returns
+        -------
+            int
+        """
+        return 2**math.ceil(math.log10(size)/math.log10(2))
+
+    def get_text_dimensions(self, text_string, font):
+        # from https://stackoverflow.com/a/46220683/9263761
+        ascent, descent = font.getmetrics()
+
+        text_width = font.getmask(text_string).getbbox()[2]
+        text_height = font.getmask(text_string).getbbox()[3] + descent
+
+        return (text_width, text_height)
+
+    def fit_text_in(self, text, width, font) -> list:
+        result = []
+
+        remaining_text = ""
+        tries = 0
+
+        while not text == remaining_text == "":
+            dimension = self.get_text_dimensions(text, font)
+
+            if dimension[0] > width:
+                splitted_text = text.split()
+                
+                if len(splitted_text) == 1:
+                    remaining_text = text[-1] + remaining_text
+                    text = text[:-1]
+                else:
+                    text = " ".join(splitted_text[:-1])
+                    remaining_text = " " + splitted_text[-1] + remaining_text            
+            else:
+                result.append(text.lstrip())
+                text = remaining_text.lstrip()
+                remaining_text = ""
+
+        return result
+
+    async def get_image(self, member):
+        font_size = 86
+        avatar_size = 320
+        avatar_pos = (48, 48, avatar_size+48, avatar_size+48)
+
+        background = Image.open("assets/images/cropped_background2.png")
+        foreground = Image.new("RGB", background.size, color="#1f2025")
+
+        mask = Image.new("L", background.size, 0)
+
+        # Draw background gradient
+        mask_draw = ImageDraw.Draw(mask)
+        mask_draw.rectangle((0, 0, background.size[0], background.size[1]-background.size[1]/3), fill="white")
+        mask = mask.filter(ImageFilter.GaussianBlur(128))
+
+        # Draw avatar hole
+        mask_draw = ImageDraw.Draw(mask)
+        mask_draw.ellipse(avatar_pos, fill="black")
+        mask = mask.filter(ImageFilter.GaussianBlur(2))
+
+        result = Image.composite(foreground, background, mask)
+
+        # Get member's avatar
+        member_avatar = member.display_avatar.with_size(self.get_ceil_power2_size(avatar_size))
+        avatar_bytes = await member_avatar.read()
+        avatar_image = Image.open(io.BytesIO(avatar_bytes))
+        if avatar_image.size[0] != avatar_size:
+            avatar_image = avatar_image.resize([avatar_size]*2)
+
+        # Create avatar mask
+        avatar_mask = Image.new("L", avatar_image.size, 0)
+        draw = ImageDraw.Draw(avatar_mask)
+        draw.ellipse((0, 0, avatar_size, avatar_size), fill=255)
+        avatar_mask = avatar_mask.filter(ImageFilter.GaussianBlur(1))
+
+        # Add avatar image
+        result.paste(avatar_image, avatar_pos, avatar_mask)
+
+        # Add text
+        font = ImageFont.truetype("assets/font/Roboto-Medium.ttf", font_size)
+        text = self.fit_text_in(self.get_image_text(member), result.size[0] - avatar_size - 48 - 24, font)
+
+        result_draw = ImageDraw.Draw(result)
+        result_draw.multiline_text((avatar_pos[2]+24, (avatar_pos[1]+avatar_pos[3])/2), "\n".join(text), font=font, fill=(255, 255, 255, 255), anchor="lm")
+
+        return result
