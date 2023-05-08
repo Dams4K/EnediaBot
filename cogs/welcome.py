110fa841d9202886dbc41347bb0328f9a6083199
dams4k <dams4k@users.noreply.github.com>
2023-05-08 16:48:05 +0200

    welcome commands localization

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -44,25 +44,40 @@ class WelcomeCog(Cog):
             if file := await self.get_welcome_file(member, welcome_config):
                 await channel.send(welcome_config.get_message(member), file=file)
 
-    welcome = SlashCommandGroup("welcome", default_member_permissions=Permissions(administrator=True), guild_only=True)
-    w_set = welcome.create_subgroup("set")
-    image = welcome.create_subgroup("image")
+    welcome = SlashCommandGroup("welcome", name_localizations={"fr": "bienvenue"}, default_member_permissions=Permissions(administrator=True), guild_only=True)
+    w_set = welcome.create_subgroup("set", name_localizations={"fr": "définir"})
+    image = welcome.create_subgroup("image", name_localizations={"fr": "image"})
 
-    @welcome.command(name="enable")
+    @welcome.command(
+        name="enable",
+        name_localizations={"fr": "activer"},
+        description="Enable the automatic welcome message",
+        description_localizations={"fr": "Activer le message de bienvenue automatique"}
+    )
     async def enable_welcome(self, ctx):
         ctx.welcome_config.enable()
 
         embed = SucceedEmbed(title="Activé", description="Les messages de bienvenue sont activés")
         await ctx.respond(embed=embed)
 
-    @welcome.command(name="disable")
+    @welcome.command(
+        name="disable",
+        name_localizations={"fr": "désactiver"},
+        description="Disable the automatic welcome message",
+        description_localizations={"fr": "Désactive le message de bienvenue automatique"}
+    )
     async def disable_welcome(self, ctx):
         ctx.welcome_config.disable()
 
         embed = DangerEmbed(title="Désactivé", description="Les messages de bienvenue sont désactivés")
         await ctx.respond(embed=embed)
 
-    @welcome.command(name="show")
+    @welcome.command(
+        name="show",
+        name_localizations={"fr": "afficher"},
+        description="Show what the welcome message looks like",
+        description_localizations={"fr": "Montre à quoi ressemble le message de bienvenue"}
+    )
     async def welcome_show(self, ctx):
         await ctx.defer(ephemeral=True)
 
@@ -71,8 +86,13 @@ class WelcomeCog(Cog):
         else:
             await ctx.respond(ctx.welcome_config.get_message(ctx.author))
 
-    @w_set.command(name="channel")
-    @option("channel", type=GuildChannel, channel_types=[ChannelType.text])
+    @w_set.command(
+        name="channel",
+        name_localizations={"fr": "salon"},
+        description="Channel where the welcome message will be sent",
+        description_localizations={"fr": "Salon où le message de bienvenue sera envoyé"}
+    )
+    @option("channel", name_localizations={"fr": "salon"}, type=GuildChannel, channel_types=[ChannelType.text])
     async def set_channel(self, ctx, channel: TextChannel):
         ctx.welcome_config.set_channel(channel)
 
@@ -81,8 +101,13 @@ class WelcomeCog(Cog):
         embed = WelcomePositiveEmbed(ctx.welcome_config, title=title, description=description)
         await ctx.respond(embed=embed)
     
-    @w_set.command(name="message")
-    @option("message", type=str, max_length=1024, required=False)
+    @w_set.command(
+        name="message",
+        name_localizations={"fr": "message"},
+        description="Message sent when someone join the server",
+        description_localizations={"fr": "Message envoyé lorsque quelqu'un rejoint le serveur"}
+    )
+    @option("message", name_localizations={"fr": "message"}, type=str, max_length=1024, required=False)
     async def set_message(self, ctx, message: str):
         ctx.welcome_config.set_message(message)
 
@@ -91,8 +116,13 @@ class WelcomeCog(Cog):
         embed = WelcomePositiveEmbed(ctx.welcome_config, title=title, description=description)
         await ctx.respond(embed=embed)
 
-    @image.command(name="font_size")
-    @option("size", type=int)
+    @image.command(
+        name="font_size",
+        name_localizations={"fr": "taille_de_police"},
+        description="Change the font size of the text on the image",
+        description_localizations={"fr": "Change la taille de la police du texte sur l'image"}
+    )
+    @option("size", name_localizations={"fr": "taille"}, type=int)
     async def image_font_size(self, ctx, size: int):
         await ctx.defer()
 
@@ -100,8 +130,13 @@ class WelcomeCog(Cog):
         if file := await self.get_welcome_file(ctx.author, ctx.welcome_config):
             await ctx.respond(ctx.welcome_config.get_message(ctx.author), file=file)
     
-    @image.command(name="avatar_size")
-    @option("size", type=int)
+    @image.command(
+        name="avatar_size",
+        name_localizations={"fr": "taille_avatar"},
+        description="Change the avatar size on the image",
+        description_localizations={"fr": "Change la taille de l'avatar sur l'image"}
+    )
+    @option("size", name_localizations={"fr": "taille"}, type=int)
     async def image_avatar_size(self, ctx, size: int):
         await ctx.defer()
 
@@ -109,7 +144,12 @@ class WelcomeCog(Cog):
         if file := await self.get_welcome_file(ctx.author, ctx.welcome_config):
             await ctx.respond(ctx.welcome_config.get_message(ctx.author), file=file)
     
-    @image.command(name="avatar_pos")
+    @image.command(
+        name="avatar_pos",
+        name_localizations={"fr": "position_avatar"},
+        description="Change the position of the avatar on the image",
+        description_localizations={"fr": "Change la position de l'avatar sur l'image"}
+    )
     @option("x", type=int)
     @option("y", type=int)
     async def image_avatar_pos(self, ctx, x: int, y: int):
@@ -119,8 +159,13 @@ class WelcomeCog(Cog):
         if file := await self.get_welcome_file(ctx.author, ctx.welcome_config):
             await ctx.respond(ctx.welcome_config.get_message(ctx.author), file=file)
 
-    @image.command(name="text")
-    @option("text", type=str, max_length=1024)
+    @image.command(
+        name="text",
+        name_localizations={"fr": "texte"},
+        description="Change the text of the image",
+        description_localizations={"fr": "Change le texte de l'image"}
+    )
+    @option("text", name_localizations={"fr": "texte"}, type=str, max_length=1024)
     async def image_text(self, ctx, text: str):
         await ctx.defer()
 
@@ -128,7 +173,12 @@ class WelcomeCog(Cog):
         if file := await self.get_welcome_file(ctx.author, ctx.welcome_config):
             await ctx.respond(ctx.welcome_config.get_message(ctx.author), file=file)
 
-    @image.command(name="text_pos")
+    @image.command(
+        name="text_pos",
+        name_localizations={"fr": "position_texte"},
+        description="Change the position of the text of the image",
+        description_localizations={"fr": "Change la position de du texte de l'image"}
+    )
     @option("x", type=int)
     @option("y", type=int)
     async def image_text_pos(self, ctx, x: int, y: int):
@@ -138,8 +188,13 @@ class WelcomeCog(Cog):
         if file := await self.get_welcome_file(ctx.author, ctx.welcome_config):
             await ctx.respond(ctx.welcome_config.get_message(ctx.author), file=file)
 
-    @image.command(name="upload")
-    async def image_upload(self, ctx):
+    @image.command(
+        name="import",
+        name_localizations={"fr": "importer"},
+        description="Import a background image that will be used by the bot",
+        description_localizations={"fr": "Importe une image de fond qui sera utilisée par le robot"}
+    )
+    async def image_import(self, ctx):
         await ctx.respond("Envoi l'image que tu veux utiliser")
 
         def has_file(m: Message):
