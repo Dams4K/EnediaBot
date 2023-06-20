ac5e6e267e92aa767b546c963c76d40a8183630a
Dams4K <dams4k@users.noreply.github.com>
2023-06-20 18:01:42 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -18,13 +18,18 @@ while not packages_installation_done:
 if not os.path.exists("datas"):
     os.mkdir("datas")
 
+write_bot_file = True
 if os.path.exists("datas/bot.json"):
+    write_bot_file = False
     overwrite = input("Le fichier bot.json existe déjà, voulez-vous quand-même continuer ? (o/N)")
     
     if overwrite.lower() in ["o", "y", "oui", "yes"]:
-        bot_token = input("Token du bot > ")
-        with open("datas/bot.json", "w") as f:
-            json.dump({
-                "bot_token": bot_token
-            }, f)
+        write_bot_file = True
+
+if write_bot_file:
+    bot_token = input("Token du bot > ")
+    with open("datas/bot.json", "w") as f:
+        json.dump({
+            "bot_token": bot_token
+        }, f)
         
